9c8448ea7c0516f7987bc5f74347930ad01fac92
khanguslee <khanguslee@gmail.com>
2019-02-10 17:01:14 +1100

    Pass in argument to set number of requests

--- HTTP/client.js
+++ HTTP/client.js
@@ -1,4 +1,21 @@
 const request = require('request-promise-native');
+const { ArgumentParser } = require('argparse');
+
+const argumentParser = new ArgumentParser({
+  version: '1.0.0',
+  addHelp: true,
+  description: 'HTTP client program',
+});
+
+argumentParser.addArgument(
+  ['-n', '--number'],
+  {
+    help: 'Number of requests',
+    defaultValue: 1,
+  },
+);
+
+const args = argumentParser.parseArgs();
 
 const serverAddress = 'http://127.0.0.1:3000';
 
@@ -10,10 +27,12 @@ const clientData = {
   json: true,
 };
 
-request(clientData)
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.error(error);
-  })
+for (let index = 0; index < args.number; index += 1) {
+  request(clientData)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+}
