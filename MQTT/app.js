13cda367716fe61ccef2444b8f0ca3449d21fcb7
khanguslee <khanguslee@gmail.com>
2019-02-10 18:27:09 +1100

    Add argument to specify number of requests

--- MQTT/app.js
+++ MQTT/app.js
@@ -1,12 +1,31 @@
 const mqtt = require('mqtt');
+const { ArgumentParser } = require('argparse');
+
+const argumentParser = new ArgumentParser({
+  version: '1.0.0',
+  addHelp: true,
+  description: 'MQTT client program',
+});
+
+argumentParser.addArgument(
+  ['-n', '--number'],
+  {
+    help: 'Number of requests',
+    defaultValue: 1,
+  },
+);
+
+const args = argumentParser.parseArgs();
 
 const client = mqtt.connect('mqtt://localhost:1883');
 
 client.on('connect', function() {
     client.subscribe('data');
 
-    const currentTime = Date.now();
-    client.publish('data', currentTime.toString());
+    for (let index = 0; index < args.number; index += 1) {
+        const currentTime = Date.now();
+        client.publish('data', currentTime.toString());
+    }
     client.end();
 })
 
